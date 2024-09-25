068ad77b1ae635756564f53d9997fbdd0bf50f4e
Bailey <priest2105@gmail.com>
2024-09-25 13:18:36 +0100

    console

---
+++ 01-variables-data-types/01-console/super.js
@@ -0,0 +1,31 @@
+console.log(909);
+
+console.log('Helllo world');
+
+console.log(20, 'Hello', true);
+
+
+const x = 100;
+
+
+console.log(x);
+
+
+console.error('Alert');
+
+console.warn('warning');
+
+
+console.table({ name : 'Brad', email: 'brad@gmail.com'})
+
+
+console.group('simple');
+console.log(x);
+console.error('Alert');
+console.warn('warning');
+
+
+const styles = 'padding:10px; background-color: white; color: greeen'
+
+
+console.log('Hello world', styles);
---
+++ 01-variables-data-types/02-comments/super.js
@@ -0,0 +1,40 @@
+
+// This is  single luine of code
+
+
+
+/** Multi line 
+comment 
+**/
+
+console.log(909);
+
+console.log('Helllo world');
+
+console.log(20, 'Hello', true);
+
+
+const x = 100;
+
+
+console.log(x);
+
+
+console.error('Alert');
+
+console.warn('warning');
+
+
+console.table({ name : 'Brad', email: 'brad@gmail.com'})
+
+
+console.group('simple');
+console.log(x);
+console.error('Alert');
+console.warn('warning');
+
+
+const styles = 'padding:10px; background-color: white; color: greeen'
+
+
+console.log('Hello world', styles);

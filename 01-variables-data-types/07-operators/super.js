5f4b219124c76cd05320e4c812580f69463c8268
Bailey <priest2105@gmail.com>
2024-09-26 08:58:43 +0100

    operators

---
+++ 01-variables-data-types/07-operators/super.js
@@ -0,0 +1,59 @@
+// Arithmetic Operator 
+
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+
+
+// Concatencation 
+
+x = 'Hello ' + ' World'
+
+
+// Exponent Operator 
+
+x = 2 * 3
+
+// Increment 
+x = 1
+x = x + 1;
+x = x++;
+
+
+
+
+// Decrement 
+x = 1
+x = x - 1;
+x = x--;
+
+
+
+// Assignemt Operators
+
+x = 10
+
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+x **= 5;
+
+
+
+// Comparison Operators 
+
+x = 2 == 2;
+x = 2 === 2;
+
+
+
+console.log(x)
+
+

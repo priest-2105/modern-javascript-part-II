4c2131c598fd92793abe4de00d0cce663048229a
Bailey <priest2105@gmail.com>
2024-09-26 08:31:30 +0100

    data-tyes || Functiona

--- 01-variables-data-types/04-datatypes/super.js
+++ 01-variables-data-types/04-datatypes/super.js
@@ -29,9 +29,9 @@ const id = Symbol('id')
 
 const n = 900291844922955n;
 
-const output = myName;
+// const output = myName;
 
-console.log(output, typeof output); 
+// console.log(output, typeof output); 
 
 
 
@@ -40,3 +40,19 @@ console.log(output, typeof output);
 
 
 // Refrence Types 
+
+
+const numbers = [1, 2, 3, 5];
+
+const  person = {
+    name: 'Brad'
+}
+
+function sayHello() {
+    console.log('Hello');
+    
+}
+
+const output = myName;
+
+console.log(output, typeof output); 

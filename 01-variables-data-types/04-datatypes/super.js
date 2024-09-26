7554f82b8ae2da0fb2fd71ddede46a084d664c7e
Bailey <priest2105@gmail.com>
2024-09-26 08:27:35 +0100

    data-tyes- undefine

--- 01-variables-data-types/04-datatypes/super.js
+++ 01-variables-data-types/04-datatypes/super.js
@@ -4,4 +4,22 @@
 
 const firstName = 'Sara';
 
-console.log(firstName, typeof firstName);
+
+// Number 
+const age = 30;
+const temp = 98.9;
+
+
+// Boolean 
+const aptNumber = null
+
+// undefined 
+
+let myName;
+
+// Symbol 
+
+
+const output = myName;
+
+console.log(output, typeof output); 

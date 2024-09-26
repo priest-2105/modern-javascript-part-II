a3f48778e91a02e2c9cdcaae3363b01a373326b7
Bailey <priest2105@gmail.com>
2024-09-26 23:50:53 +0100

    challenge ✨✨

--- 01-variables-data-types/10-challenge/super.js
+++ 01-variables-data-types/10-challenge/super.js
@@ -1,52 +1,7 @@
-let x;
+const myString = 'developer';
 
-const name = 'Bailey';
-const age = 30;
-
-// String Concatenation
-x = 'Hello , my name is' + name + 'and i am ' + age + ' years old';
-
-// Template Literals 
-x = `Hello , my name is  ${name}  and i am ${age} years old`;
+// const myNewString = myString.split('')
+const myNewString  = myString[0].toUpperCase() + myString.slice(1)
 
 
-// String Properties and Methods
-const s = 'Hello world';
-
-x = typeof s ;
-
-x = s.length;
-
-// Accessing Value by key
-x = s[0];
-
-x = s.__proto__;
-
-x = s.toUpperCase();
-
-x = s.toLowerCase();
-
-x = s.charAt(0);
-
-x = s.indexOf('o');
-
-x = s.substring(0, 4);
-
-x = s.substring(7);
-
-x = s.slice(11);
-
-x = '                 Hello world'
-
-x = s.trim();
-
-x = x.replace('H', 'F');
-
-x = x.includes('world');
-
-x = s.valueOf();
-
-x = s.split(' ');
-
-
-console.log(x);
+console.log('Question', myString, 'Result', myNewString);

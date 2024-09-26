bce8339cc539bc4f485e2c582fbb5e05a8b01b23
Bailey <priest2105@gmail.com>
2024-09-26 08:15:26 +0100

    data-tyes

---
+++ 01-variables-data-types/04-datatypes/super.js
@@ -0,0 +1,93 @@
+// Ways to declare avariable 
+//  var, let, const 
+
+let firstName = 'John';
+let lastName = 'Doe';
+
+console.log(firstName + ' ' + lastName);
+
+
+let age = 30;
+
+
+console.log(age);
+
+
+// Naming Conventions
+
+//  - Only letters, Numbers , underscores and dolla signs
+//  - can't start with a number
+
+
+
+// Multi-World Formating 
+// firstName = camelCase
+// first_name = underscore
+//  FirstName = PascelCase
+//  firstname = lowercase
+
+
+
+// Reassiging Variables
+
+age = 31;
+
+console.log(age);
+
+
+let score;
+
+
+score = 1
+
+
+console.log(score);
+
+
+if (true){
+     score = score + 1
+}
+
+
+console.log(score);
+
+
+
+const x = 100;
+
+
+const arr = [1, 2, 3, 4]
+
+
+arr.push(5);
+
+
+console.log(arr);
+
+
+
+const person = {
+    name: 'Brad',
+}
+
+person.name = 'John';
+
+person.email = 'brad@gmail.com';
+
+
+console.log(person);
+
+
+
+
+// Declare Multiple Valuyes at once 
+
+let a, b, c;
+
+const d = 20,
+    e = 20,
+    f = 30;
+
+
+console.log(d);
+console.log(a);

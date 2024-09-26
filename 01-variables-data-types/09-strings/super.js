83a72b07f9d95d673cca5d1f516788db38909695
Bailey <priest2105@gmail.com>
2024-09-26 18:37:33 +0100

    template literals

---
+++ 01-variables-data-types/09-strings/super.js
@@ -0,0 +1,15 @@
+let x;
+
+const name = 'Bailey';
+const age = 30;
+
+// String Concatenation
+x = 'Hello , my name is' + ${name} + 'and i am ' + age + ' years old'
+
+// Template Literals 
+x = `Hello , my name is  ${name}  and i am ${age} years old`
+
+
+
+
+console.log(x);

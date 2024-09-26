548f27110ab9fc01764da00c33cd9bfd11f2f0c7
Bailey <priest2105@gmail.com>
2024-09-26 08:37:52 +0100

    stack || heap

---
+++ 01-variables-data-types/04-stack-heap/super.js
@@ -0,0 +1,58 @@
+//  Data Types
+
+
+
+// primtive Types 
+
+// string
+
+const firstName = 'Sara';
+
+
+// Number 
+const age = 30;
+const temp = 98.9;
+
+
+// Boolean 
+const aptNumber = null
+
+// undefined 
+
+let myName;
+
+// Symbol 
+
+const id = Symbol('id')
+
+// Big int 
+
+const n = 900291844922955n;
+
+// const output = myName;
+
+// console.log(output, typeof output); 
+
+
+
+
+
+
+
+// Refrence Types 
+
+
+const numbers = [1, 2, 3, 5];
+
+const  person = {
+    name: 'Brad'
+}
+
+function sayHello() {
+    console.log('Hello');
+    
+}
+
+const output = myName;
+
+console.log(output, typeof output); 

4f716dafce9867a8263684accc609fdcd8331423
Bailey <priest2105@gmail.com>
2024-09-26 19:05:05 +0100

    string methods

--- 01-variables-data-types/09-strings/super.js
+++ 01-variables-data-types/09-strings/super.js
@@ -4,10 +4,45 @@ const name = 'Bailey';
 const age = 30;
 
 // String Concatenation
-x = 'Hello , my name is' + ${name} + 'and i am ' + age + ' years old'
+x = 'Hello , my name is' + name + 'and i am ' + age + ' years old';
 
 // Template Literals 
-x = `Hello , my name is  ${name}  and i am ${age} years old`
+x = `Hello , my name is  ${name}  and i am ${age} years old`;
+
+
+// String Properties and Methods
+const s = 'Hello world';
+
+x = typeof s ;
+
+x = s.length;
+
+// Accessing Value by key
+x = s[0];
+
+x = s.__proto__;
+
+x = s.toUpperCase();
+
+x = s.toLowerCase();
+
+x = s.charAt(0);
+
+x = s.indexOf('o');
+
+x = s.substring(0, 4);
+
+x = s.substring(7);
+
+x = s.slice(11);
+
+x = '                 Hello world'
+
+x = s.trim();
+
+x = x.replace('H', 'F');
+
+x = x.includes('world');
 
 
 

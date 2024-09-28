d3f234174da4bd6888504d37976468a31ff2016b
Bailey <priest2105@gmail.com>
2024-09-28 18:11:35 +0100

    section one complete ✔

--- 01-variables-data-types/15-date-objects/super.js
+++ 01-variables-data-types/15-date-objects/super.js
@@ -1,2 +1,48 @@
 let x;
+let d = new Date();
 
+x = d.toString();
+
+x = d.getTime();
+x = d.valueOf();
+
+x = d.getFullYear();
+
+x = d.getMonth() + 1;
+
+x = d.getDate();
+
+x = d.getDay();
+
+x = d.getHours();
+
+x = d.getMinutes();
+
+x = d.getSeconds();
+
+x = d.getMilliseconds();
+
+x = `${d.getFullYear()} ${d.getMonth()} ${d.getDate()}`
+
+
+x = Intl.DateTimeFormat('en-US').format(d)
+x = Intl.DateTimeFormat('en-GB').format(d)
+x = Intl.DateTimeFormat('default').format(d)
+
+x = Intl.DateTimeFormat('default', { month: 'long' }).format(d);
+
+x = d.toLocaleString('default', { month: 'short'});
+
+x = d.toLocaleString('default', { 
+    month: 'long',
+    weekday: 'long',
+    year: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZone: 'America/Los_Angeles',
+});
+
+
+console.log(x);

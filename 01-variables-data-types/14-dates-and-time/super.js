6ca4609de06dc40d902316f0fb6f0ef9f9cc52c4
Bailey <priest2105@gmail.com>
2024-09-28 18:02:04 +0100

    date amd time 0bjects

--- 01-variables-data-types/14-dates-and-time/super.js
+++ 01-variables-data-types/14-dates-and-time/super.js
@@ -7,9 +7,20 @@ d = d.toString();
 
 d = new Date(2021, 11, 10, 12, 20, 12);
 
-d = new Date('2021', '07', '10');
+d = new Date('2021-07-10T12:30:12');
+d = new Date('07/10/2021 12:30:12');
+d = new Date('2022-07-10');
+d = new Date('07-10-2022');
 
 
-console.log(typeof d);
+d = Date.now();
+
+d = new Date();
+d = d.getTime();
+d = d.valueOf();
+
+d = new Date('1727542850247')
+
+console.log(d);
 
 

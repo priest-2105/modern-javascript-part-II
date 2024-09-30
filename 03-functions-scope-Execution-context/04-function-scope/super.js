063c35c7af6fef80c8213e0b54826a873976f567
Bailey <priest2105@gmail.com>
2024-09-30 05:25:36 +0100

    scopes

---
+++ 03-functions-scope-Execution-context/04-function-scope/super.js
@@ -0,0 +1,33 @@
+const x = 100;
+const foo = 1
+var bar = 2;
+
+if (true){
+    const y = 200;
+    console.log(x + y);
+}
+
+// console.log(x + y);
+
+for (let i = 0; i <= 1; i++ ){
+   console.log(i);
+}
+
+console.log(i);
+
+if(true){
+    const a = 500;
+    let b = 600;
+    var c = 700;
+}
+
+console.log(a, b, c);
+
+function run(){
+    var d = 100;
+    console.log(d);
+}
+
+run();
+
+console.log(d);

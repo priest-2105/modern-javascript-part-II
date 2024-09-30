ef13ee2e34ddfbdd92c2a0a224689da09c4f1956
Bailey <priest2105@gmail.com>
2024-09-30 12:34:00 +0100

    challenge

--- 03-functions-scope-Execution-context/09-challenge(function)/super.js
+++ 03-functions-scope-Execution-context/09-challenge(function)/super.js
@@ -1,5 +1,27 @@
+// Challenge 2 
+const getCelsiusfarahn = fahren =>  (fahren - 32) * (5/9)
 
-const getCelsiusfarahn = fahren => { return (fahren - 32) * (5/9)}
-
+// const getCelsiusfarahn = fahren => { return (fahren - 32) * (5/9)}
 
 console.log(getCelsiusfarahn(50));
+
+
+// Challenge Two
+const minMax = (arr) => {
+    return({
+        min : Math.min(...arr),
+        max : Math.max(...arr)    
+    })
+}
+
+console.log(minMax([1, 2, 3, 4, 5]));
+
+
+// Challenge Three
+
+(function add(length, width){
+        const area = length * width;
+        console.log(`The area of th ereactangle with  a lenght of ${length} and a width of ${width} is ${area}`); 
+})(10, 51);
+
+

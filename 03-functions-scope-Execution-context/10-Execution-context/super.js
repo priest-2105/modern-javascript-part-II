77c6e915af0a926ac8b6af842d3b595094c4ff58
fawazBailey <fawaz.b@ihealthwellness.org>
2024-10-01 23:09:10 +0100

    callstaxk

--- 03-functions-scope-Execution-context/10-Execution-context/super.js
+++ 03-functions-scope-Execution-context/10-Execution-context/super.js
@@ -1,1 +1,14 @@
+ const x = 100;
+ const y = 50;
+
+
+ function getSum(n1, n2){
+    const sum = n1 + n2;
+    return sum;
+ }
+
+ const sum1 = getSum(x, y);
+ const sum2 = getSum(`0`, 5);
+
+ console.log(sum1, sum2);
  

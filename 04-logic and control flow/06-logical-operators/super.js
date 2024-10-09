6e51b7efbc9887ae927f41434d5d399c9a59c5e3
fawazBailey <fawaz.b@ihealthwellness.org>
2024-10-10 00:34:45 +0100

    commit

---
+++ 04-logic and control flow/06-logical-operators/super.js
@@ -0,0 +1,23 @@
+ console.log(10>  20 && 30 > 15 && 40 > 30);
+
+ console.log(10 > 20 || 30 > 15);
+ 
+
+
+//  && - will return firs falsy value or h elast value 
+
+let a;
+
+
+
+
+a = 10 && 20;
+a = 10 && 20 && 30;
+a = 10 && 0 && 30;
+a = 10 && '' && 20 && 30;
+
+
+
+
+console.log(a);
+

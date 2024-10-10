dfa4afc12bada11fc8159ae6494b4a03e8a17228
fawazBailey <fawaz.b@ihealthwellness.org>
2024-10-10 01:33:30 +0100

    Loops

---
+++ 05-Loops-High-Order-Array-Methods/02-while-loop/super.js
@@ -0,0 +1,39 @@
+let i = 0;
+
+
+// while ( i <= 20 ) {
+//     console.log('Number' + i )
+//     i++;
+// }
+
+
+
+// Loop over arrays
+
+const arr = [10, 20, 30, 40]
+
+while (1 > arr.length) {
+    console.log(arr[i]);
+    i++
+}
+
+
+// nesting while loops 
+
+// while (i <= 5) {
+//     console.log('Number ' + i);
+//     let j = 1;
+
+//     while (j <= 5){
+//         console.log(`${i} * ${j} = ${i * j}`);
+//         j++;
+//     }
+//         i++;
+// }
+
+
+
+do{
+    console.log('number ', i);
+    i++;
+}while( i <= 20 );

24a644714518679c221ecadbd95b672e2138372f
priest-2105 <priest2105@gmail.com>
2024-10-30 01:33:11 +0100

    creating elemnts ✨✨

--- 06-Document-object-model/07-tcreating-elements/super.js
+++ 06-Document-object-model/07-tcreating-elements/super.js
@@ -1,45 +1,9 @@
-let output;
+const div = document.createElement('div');
 
-//  Get child Element
+div.className = 'my-element';
+div.id = 'my-element';
+div.setAttribute('title', 'My Element');
 
-const parent = document.querySelector('.parent');
+div.innerText = 'Hello world';
 
-output = parent.childNodes;
-output = parent.childNodes[0].textContent;
-output = parent.childNodes[0].nodeName;
-output = parent.childNodes[3].textContent;
-output = parent.childNodes[3].innerHTML;
-output = parent.childNodes[3].outerHTML;
-
-
-output = parent.childNodes[3].innerText = 'Child ONE';
-output = parent.childNodes[3].style.color = 'red';
-
-
-output = parent.firstChild;
-output = parent.lastChild;
-
-parent.lastChild.textContent = 'Hello';
-
-// parent Node
-
-const child = document.querySelector('.child');
-
-output = child.parentNode;
-output = child.parentElement;
-
-
-child.parentNode.style.backgroundColor = '#ccc';
-child.parentNode.style.pading = '10px';
-
-
-// Sibling 
-
-const secondItem = document.querySelector('.child:nth-child(2)');
-
-
-output = secondItem.nextSibling;
-output = secondItem.previousSibling;
-
-
-console.log(output);
+console.log(div);

884cd57908abc56bb8446ef74a78ad74f6361f39
priest-2105 <priest2105@gmail.com>
2024-10-30 01:56:20 +0100

    creating elemnts ✨✨

--- 06-Document-object-model/09-multiple-functions/super.js
+++ 06-Document-object-model/09-multiple-functions/super.js
@@ -1,20 +1,4 @@
-//  Quick and dirty 
-
-function createListItem(item){
-    const li = document.createElement('li');
-
-    li.innerHTML = `
-          ${item}
-          <button class="remove-item btn-link text-red">
-            <i class="fa-solid fa-xmark"></i>
-          </button>
-    `
-
-    document.querySelector('.items').appendChild(li);
-}
-
-
-// Clean % performant
+ 
 
 function createNewItem(item){
 
@@ -39,7 +23,13 @@ function createNewItem(item){
 }
 
 
+function createButton(classes){
+  
+}
+
+function createIcon(classes){
+  
+}
 
 
-createListItem('Eggs');
 createNewItem('Cheese');
